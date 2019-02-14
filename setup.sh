39d999610ecaded712f49ed0fe378d6034a9a078
Robert McConnell <robert.mcconnell@protonmail.com>
2019-02-14 11:29:53 +0000

    Create setup.sh
    Installs python, pip, virtualenv, tensorflow, pil, numpy, matplotlib and set PYTHONPATH

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# install python in debian
+sudo apt install -y python-dev python-pip
+sudo pip install -U virtualenv  # system-wide install
+
+# create virtual environment
+virtualenv --system-site-packages -p python2.7 ./venv
+
+source ./venv/bin/activate  # sh, bash, ksh, or zsh
+pip install --upgrade pip
+
+pip list  # show packages installed within the virtual environment
+
+# install tensorflow
+pip install --upgrade tensorflow
+python -c "import tensorflow as tf; tf.enable_eager_execution(); print(tf.reduce_sum(tf.random_normal([1000, 1000])))"
+
+sudo apt-get install -y python-pil python-numpy
+sudo pip install matplotlib
+
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+
+deactivate  # don't exit until you're done using TensorFlow
