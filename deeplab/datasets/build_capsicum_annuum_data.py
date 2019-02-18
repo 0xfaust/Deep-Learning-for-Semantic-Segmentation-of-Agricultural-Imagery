d697aca23d194cf6e1c8d54664278d0bc3fc2a49
Robert McConnell <robert.mcconnell@protonmail.com>
2019-02-18 12:01:20 +0000

    Add scripts to download capsicum annuum dataset and generate tfrecord
    Dataset source: https://data.4tu.nl/repository/uuid:884958f5-b868-46e1-b3d8-a0b5d91b02c0?key=authorId%3A%22orcid%3A0000-0002-9216-1607%22

---
+++ deeplab/datasets/build_capsicum_annuum_data.py
@@ -0,0 +1,107 @@
+import math
+import os.path
+import sys
+import build_data
+import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
+
+def del_all_flags(FLAGS):
+    flags_dict = FLAGS._flags()
+    keys_list = [keys for keys in flags_dict]
+    for keys in keys_list:
+        FLAGS.__delattr__(keys)
+
+del_all_flags(tf.flags.FLAGS)
+
+tf.app.flags.DEFINE_string('image_folder',
+                           './capsicum_annuum/empirical_image_color',
+                           'Folder containing images.')
+
+tf.app.flags.DEFINE_string(
+    'semantic_segmentation_folder',
+    './capsicum_annuum/segmentation_class',
+    'Folder containing semantic segmentation annotations.')
+
+tf.app.flags.DEFINE_string(
+    'list_folder',
+    './capsicum_annuum/image_sets',
+    'Folder containing lists for training and validation')
+
+tf.app.flags.DEFINE_string(
+    'image_format',
+    "png",
+    'Format of images.')
+
+tf.app.flags.DEFINE_string(
+    'label_format',
+    "png",
+    'Format of labels.')
+
+tf.app.flags.DEFINE_string(
+    'output_dir',
+    './capsicum_annuum/tfrecord',
+    'Path to save converted SSTable of TensorFlow examples.')
+
+
+_NUM_SHARDS = 4
+
+
+def _convert_dataset(dataset_split):
+  """Converts the specified dataset split to TFRecord format.
+
+  Args:
+    dataset_split: The dataset split (e.g., train, test).
+
+  Raises:
+    RuntimeError: If loaded image and label have different shape.
+  """
+  dataset = os.path.basename(dataset_split)[:-4]
+  sys.stdout.write('Processing ' + dataset)
+  filenames = [x.strip('\n') for x in open(dataset_split, 'r')]
+  num_images = len(filenames)
+  num_per_shard = int(math.ceil(num_images / float(_NUM_SHARDS)))
+
+  image_reader = build_data.ImageReader('png', channels=3)
+  label_reader = build_data.ImageReader('png', channels=1)
+
+  for shard_id in range(_NUM_SHARDS):
+    output_filename = os.path.join(
+        FLAGS.output_dir,
+        '%s-%05d-of-%05d.tfrecord' % (dataset, shard_id, _NUM_SHARDS))
+    with tf.python_io.TFRecordWriter(output_filename) as tfrecord_writer:
+      start_idx = shard_id * num_per_shard
+      end_idx = min((shard_id + 1) * num_per_shard, num_images)
+      for i in range(start_idx, end_idx):
+        sys.stdout.write('\r>> Converting image %d/%d shard %d' % (
+            i + 1, len(filenames), shard_id))
+        sys.stdout.flush()
+        # Read the image.
+        image_filename = os.path.join(
+            FLAGS.image_folder, filenames[i] + '.png') #+ FLAGS.image_format)
+        image_data = tf.gfile.FastGFile(image_filename, 'rb').read()
+        height, width = image_reader.read_image_dims(image_data)
+        # Read the semantic segmentation annotation.
+        seg_filename = os.path.join(
+            FLAGS.semantic_segmentation_folder,
+            filenames[i] + '.png') #+ FLAGS.label_format)
+        seg_data = tf.gfile.FastGFile(seg_filename, 'rb').read()
+        seg_height, seg_width = label_reader.read_image_dims(seg_data)
+        if height != seg_height or width != seg_width:
+          raise RuntimeError('Shape mismatched between image and label.')
+        # Convert to tf example.
+        example = build_data.image_seg_to_tfexample(
+            image_data, filenames[i], height, width, seg_data)
+        tfrecord_writer.write(example.SerializeToString())
+    sys.stdout.write('\n')
+    sys.stdout.flush()
+
+
+def main(unused_argv):
+  dataset_splits = tf.gfile.Glob(os.path.join(FLAGS.list_folder, '*.txt'))
+  for dataset_split in dataset_splits:
+    _convert_dataset(dataset_split)
+
+
+if __name__ == '__main__':
+  tf.app.run()
---
+++ deeplab/datasets/download_and_convert_capsicum_annuum.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+set -e
+
+EXP=$1
+DATASET_DIR=$2
+
+# empirical data
+EMPIRICAL_BASE_URL='https://data.4tu.nl/bulk/uuid_884958f5-b868-46e1-b3d8-a0b5d91b02c0'
+EMPIRICAL_FILENAME_DATA='empirical_image_color.zip'
+EMPIRICAL_FILENAME_LABELS='empirical_label_class_grayscale.zip'
+
+# synthetic data
+SYNTHETIC_BASE_URL='https://data.4tu.nl/bulk/uuid_884958f5-b868-46e1-b3d8-a0b5d91b02c0'
+SYNTHETIC_FILENAME_DATA='synthetic_image_color.zip'
+SYNTHETIC_FILENAME_LABELS='synthetic_label_class_grayscale.zip'
+
+# setup directories
+CAPSICUM_ANNUUM_DIR="$DATASET_DIR/capsicum_annuum"
+LIST_DIR="$CAPSICUM_ANNUUM_DIR/image_sets"
+ANNOTATED_DIR="$CAPSICUM_ANNUUM_DIR/segmentation_class"
+INIT_DIR="$CAPSICUM_ANNUUM_DIR/init_models"
+EXP_DIR="$CAPSICUM_ANNUUM_DIR/exp"
+
+if [[ $EXP == "A" ]]; then
+  IMAGE_DIR="$CAPSICUM_ANNUUM_DIR/synthetic_image_color"
+  GROUND_TRUTH_DIR="$CAPSICUM_ANNUUM_DIR/synthetic_label_class_grayscale/synthetic_label_class_all_grayscale"
+  BASE_URL=$SYNTHETIC_BASE_URL
+  FILENAME_DATA=$SYNTHETIC_FILENAME_DATA
+  FILENAME_LABELS=$SYNTHETIC_FILENAME_LABELS
+  EXP_ID="$EXP_DIR/a"
+elif [[ $EXP == "B" ]]; then
+  echo "Experiment B and E are not implemented yet."
+  exit 1
+elif [[ $EXP == "C" ]]; then
+  IMAGE_DIR="$CAPSICUM_ANNUUM_DIR/empirical_image_color"
+  GROUND_TRUTH_DIR="$CAPSICUM_ANNUUM_DIR/empirical_label_class_grayscale/empirical_label_class_all_grayscale"
+  BASE_URL=$EMPIRICAL_BASE_URL
+  FILENAME_DATA=$EMPIRICAL_FILENAME_DATA
+  FILENAME_LABELS=$EMPIRICAL_FILENAME_LABELS
+  EXP_ID="$EXP_DIR/c"
+elif [[ $EXP == "D" ]]; then
+  IMAGE_DIR="$CAPSICUM_ANNUUM_DIR/empirical_image_color"
+  GROUND_TRUTH_DIR="$CAPSICUM_ANNUUM_DIR/empirical_label_class_grayscale/empirical_label_class_all_grayscale"
+  BASE_URL=$EMPIRICAL_BASE_URL
+  FILENAME_DATA=$EMPIRICAL_FILENAME_DATA
+  FILENAME_LABELS=$EMPIRICAL_FILENAME_LABELS
+  EXP_ID="$EXP_DIR/d"
+elif [[ $EXP == "E" ]]; then
+  echo "Experiment B and E are not implemented yet."
+  exit 1
+fi
+
+TRAIN_LOGDIR="$EXP_ID/train"
+EVAL_LOGDIR="$EXP_ID/eval"
+VIS_LOGDIR="$EXP_ID/vis"
+EXPORT_DIR="$EXP_ID/export"
+TF_RECORD_DIR="$CAPSICUM_ANNUUM_DIR/tfrecord"
+
+mkdir -p "${CAPSICUM_ANNUUM_DIR}"
+mkdir -p "${LIST_DIR}"
+mkdir -p "${ANNOTATED_DIR}"
+mkdir -p "${INIT_DIR}"
+mkdir -p "${TRAIN_LOGDIR}"
+mkdir -p "${EVAL_LOGDIR}"
+mkdir -p "${VIS_LOGDIR}"
+mkdir -p "${EXPORT_DIR}"
+mkdir -p "${TF_RECORD_DIR}"
+
+cd "${CAPSICUM_ANNUUM_DIR}"
+
+
+# Helper function to download dataset.
+download(){
+  local BASE_URL=${1}
+  local FILENAME=${2}
+
+  if [[ ! -f "${FILENAME}" ]]; then
+    echo "Downloading ${FILENAME} to ${CAPSICUM_ANNUUM_DIR}"
+    wget -q -nd -c  "${BASE_URL}/${FILENAME}"
+  fi
+}
+
+# Download the images.
+download "${BASE_URL}" "${FILENAME_DATA}"
+download "${BASE_URL}" "${FILENAME_LABELS}"
+
+# Helper function to unpack dataset.
+uncompress() {
+  local BASE_URL=${1}
+  local FILENAME=${2}
+
+  echo "Uncompressing ${FILENAME}"
+  unzip "${FILENAME}"
+}
+
+# Uncompress the images.
+uncompress "${BASE_URL}" "${FILENAME_DATA}"
+uncompress "${BASE_URL}" "${FILENAME_LABELS}"
+
+echo "Removing the color map in ground truth annotations..."
+echo "Ground truth directory: $GROUND_TRUTH_DIR"
+
+python ../remove_gt_colormap.py \
+  --original_gt_folder="$GROUND_TRUTH_DIR" \
+--output_dir="$ANNOTATED_DIR/raw"
+
+cd "${ANNOTATED_DIR}/raw"
+echo "${ANNOTATED_DIR}"
+cp *.png ../
+cd "${ANNOTATED_DIR}"
+rename 's/label_class_all_grayscale/image_color/' *.png
+
+cd "${IMAGE_DIR}"
+
+if [[ $EXP == "A" ]]; then
+  ls -v | head -8750 | cut -d '.' -f 1 > "${LIST_DIR}/train.txt"
+  ls -v | tail -49 | cut -d '.' -f 1 > "${LIST_DIR}/val.txt"
+  cat "${LIST_DIR}/train.txt" "${LIST_DIR}/val.txt" > "${LIST_DIR}/trainval.txt"
+elif [[ $EXP == "B" ]]; then
+  echo "Experiment B and E are not implemented yet."
+  exit 1
+elif [[ $EXP == "C" ]]; then
+  ls -v | head -30 | cut -d '.' -f 1 > "${LIST_DIR}/train.txt"
+  ls -v | tail -9 | cut -d '.' -f 1 > "${LIST_DIR}/val.txt"
+  cat "${LIST_DIR}/train.txt" "${LIST_DIR}/val.txt" > "${LIST_DIR}/trainval.txt"
+elif [[ $EXP == "D" ]]; then
+  ls -v | head -30 | cut -d '.' -f 1 > "${LIST_DIR}/train.txt"
+  ls -v | tail -9 | cut -d '.' -f 1 > "${LIST_DIR}/val.txt"
+  cat "${LIST_DIR}/train.txt" "${LIST_DIR}/val.txt" > "${LIST_DIR}/trainval.txt"
+elif [[ $EXP == "E" ]]; then
+  echo "Experiment B and E are not implemented yet."
+  exit 1
+fi
+
+echo "Converting Capsicum Annuum dataset..."
+cd "${DATASET_DIR}"
+python ./build_capsicum_annuum_data.py \
+  --image_folder="${IMAGE_DIR}" \
+  --semantic_segmentation_folder="${ANNOTATED_DIR}" \
+  --list_folder="${LIST_DIR}" \
+  --image_format="png" \
+--output_dir="${TF_RECORD_DIR}"
